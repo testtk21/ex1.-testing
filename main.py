7916a879cb551136a91ce323a4c6efecce32f467
Snowx555 <153617818+Snowx555@users.noreply.github.com>
2023-12-12 16:45:27 +0300

    Update main.py
    на всякий 2 раз Анисимов Виталий

--- main.py
+++ main.py
@@ -12,13 +12,37 @@ while True:
     if choice == '1':
         name = input("Введите имя пользователя: ")
         age = input("Введите возраст пользователя: ")
+        try:
+            name=str(name)
+            age=int(age)
+        except ValueError:
+            print("Ошибка неверный формат возвраста или имени")
+            continue
 
         user_data.append({'name': name, 'age': age})
 
+    elif choice =='2':
+        name = input("Введите имя пользователя для удаления:")
+        for user in user_data:
+            if user['name'] == name:
+                user_data.remove(user)
+                print(f"Пользователь {name} удален")
+                break
+            
     elif choice == '3':
         print("Информация о пользователях:")
         for user in user_data:
-            print(f"Имя: {user['name']}, Возраст: {user['age']}")
+            print(f"Имя: {name}, Возраст: {age}")
+
+    elif choice == '4':
+        if len(user_data) == 0:
+            print("Нет данных о пользователе")
+        else:
+            total_age =0
+            for user in user_data:
+                total_age += user['age']
+                average_age = total_age / len(user_data)
+                print(f"Средний возвраст пользователей: {average_age}")
 
     elif choice == '5':
         print("Выход из программы.")
